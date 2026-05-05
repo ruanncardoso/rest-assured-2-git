1f7c7547ef4dbe8437fd773c2cf61d970c128e4c
Ruan Cardoso <contato.ruanguedes@gmail.com>
2026-05-04 21:35:43 -0300

    Segundo teste

--- src/test/java/TesteBasico.java
+++ src/test/java/TesteBasico.java
@@ -2,22 +2,39 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class TesteBasico {
 
-    // Define um método de teste
     @Test
     public void testGetBooking() {
-        // Configura a URL base para as requisições da API
         RestAssured.baseURI = "https://restful-booker.herokuapp.com";
 
-        // Configura e executa a requisição GET para o endpoint "/booking/"
-        given() // Define as configurações da requisição (headers, parâmetros, etc.)
-                .header("Accept", "*/*") //adiciona o header accept
-        .when() // Indica o início da execução da requisição
-                .get("/booking/") // Especifica o endpoint a ser chamado
-        .then() // Define as validações da resposta
-                .statusCode(200) // Verifica se o status code da resposta é 200 (OK)
-                .log().all();    // Loga no console todos os detalhes da resposta (body, headers, etc.)
+        given()
+                .header("Accept", "*/*")
+                .when()
+                .get("/booking/")
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void testWithId() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+
+        given()
+                .header("Accept", "application/json")
+                .when()
+                .get("/booking/595")
+                .then()
+                .statusCode(200)
+                .body("firstname", equalTo("Josh"))
+                .body("lastname", equalTo("Allen"))
+                .body("totalprice", equalTo(111))
+                .body("depositpaid", is(true))
+                .body("bookingdates.checkin", equalTo("2018-01-01"))
+                .body("bookingdates.checkout", equalTo("2019-01-01"))
+                .body("additionalneeds", equalTo("super bowls"));
     }
 }
